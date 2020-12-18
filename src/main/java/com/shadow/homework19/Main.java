8690348373870377c8af602198628d584677027d
G <thisblackghost@gmail.com>
2020-12-18 08:34:34 +0200

    Add Class Point

--- src/main/java/com/shadow/homework19/Main.java
+++ src/main/java/com/shadow/homework19/Main.java
@@ -1,5 +1,12 @@
 package com.shadow.homework19;
 
 public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
 
+    private void run() {
+        
+    }
 }
---
+++ src/main/java/com/shadow/homework19/Point.java
@@ -0,0 +1,21 @@
+package com.shadow.homework19;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public void move(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+
+    public synchronized void moveS(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+}
