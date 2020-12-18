b9bcf5ae8fa0f9c4189b4bdaa764a133818a93b0
G <thisblackghost@gmail.com>
2020-12-18 12:56:13 +0200

    Add class WorkerVolatile and WorkerAtomic

--- src/main/java/com/shadow/homework20/Main.java
+++ src/main/java/com/shadow/homework20/Main.java
@@ -1,7 +1,6 @@
 package com.shadow.homework20;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -9,13 +8,44 @@ public class Main {
 
     public static void main(String[] args) {
         First();
+        FirstA();
+        FirstB();
+    }
+
+    private static void FirstB() {
+        int countOperation = 2000;
+        ExecutorService executorService = Executors.newFixedThreadPool(countOperation);
+        CountDownLatch start = new CountDownLatch(countOperation);
+        final WorkerAtomic workerAtomaric = new WorkerAtomic(start);
+        for (int i=0; i < countOperation; i++)
+            executorService.submit(workerAtomaric);
+        start.countDown();
+        workerAtomaric.soutCount();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
+    }
+
+    private static void FirstA() {
+        int countOperation = 2000;
+        ExecutorService executorService = Executors.newFixedThreadPool(countOperation);
+        CountDownLatch start = new CountDownLatch(countOperation);
+        final WorkerVolatile workerVolatile = new WorkerVolatile(start);
+        for (int i=0; i < countOperation; i++)
+            executorService.submit(workerVolatile);
+        start.countDown();
+        workerVolatile.soutCount();
+        executorService.shutdown();
     }
 
     private static void First() {
         int countOperation = 2000;
         ExecutorService executorService = Executors.newFixedThreadPool(countOperation);
         CountDownLatch start = new CountDownLatch(countOperation);
-        Worker worker = new Worker(start);
+        final Worker worker = new Worker(start);
         for (int i=0; i < countOperation; i++)
             executorService.submit(worker);
         start.countDown();
