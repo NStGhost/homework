14408261c350bbf07ba190bea176a70605356d77
G <thisblackghost@gmail.com>
2020-12-18 12:56:19 +0200

    Add class WorkerVolatile and WorkerAtomic

---
+++ src/main/java/com/shadow/homework20/WorkerVolatile.java
@@ -0,0 +1,22 @@
+package com.shadow.homework20;
+
+import java.util.concurrent.CountDownLatch;
+
+public class WorkerVolatile implements Runnable{
+    private final CountDownLatch countDownLatch;
+    private int count;
+
+    public WorkerVolatile(CountDownLatch countDownLatch) {
+        this.countDownLatch = countDownLatch;
+        this.count = 0;
+    }
+
+    @Override
+    public void run() {
+        count++;
+    }
+
+    public void soutCount() {
+        System.out.println("Volatile - " + count);
+    }
+}
